d4345fd31c08bf7a9e75349ee2f817879ac4abac
jiang <392711804@qq.com>
2021-12-08 13:53:01 +0800

    update move_constructor.cpp

--- cpp/src/move_constructor.cpp
+++ cpp/src/move_constructor.cpp
@@ -8,19 +8,19 @@
 
 class Buffer {
  public:
-  explicit Buffer(size_t n) : buf(new char[n]), n(n) {
-    std::cout << "constructor" << std::endl;
+  explicit Buffer(size_t n) : id_(next_id++), buf(new char[n]), n(n) {
+    std::cout << "constructor " << id_ << std::endl;
   }
-  Buffer(const Buffer& buffer) : buf(new char[buffer.n]) {
-    std::cout << "copy constructor" << std::endl;
+  Buffer(const Buffer& buffer) : id_(next_id++), buf(new char[buffer.n]) {
+    std::cout << "copy constructor" << id_ << std::endl;
     used = buffer.used.load(std::memory_order_acquire);
     n = buffer.n;
 
     memcpy(buf, buffer.buf, n);
   }
 
-  Buffer(Buffer&& buffer) noexcept : n(buffer.n) {
-    std::cout << "move constructor" << std::endl;
+  Buffer(Buffer&& buffer) noexcept : id_(next_id++), n(buffer.n) {
+    std::cout << "move constructor " << id_ << std::endl;
     used = buffer.used.load(std::memory_order_acquire);
     buf = buffer.buf;
     buffer.buf = nullptr;  // avoid double free
@@ -29,26 +29,37 @@ class Buffer {
   Buffer& operator=(Buffer&) = delete;
 
   ~Buffer() {
-    std::cout << "free" << std::endl;
+    std::cout << "free " << id_ << ": ";
+    if (buf == nullptr) {
+      std::cout << "null" << std::endl;
+    } else {
+      std::cout << (void*)buf << std::endl;
+    }
     delete[] buf;
   }
 
   char* GetBuf() const { return buf; }
+  int GetID() const { return id_; }
 
  private:
+  static int next_id;
+  int id_;
   std::atomic<int> used;
   char* buf;
   size_t n;
 };
 
+int Buffer::next_id = 0;
+
 class Allocator {
  public:
   explicit Allocator(Buffer& buffer) : buffer_(buffer) {
-    std::cout << "construct Allocator by &" << std::endl;
+    std::cout << "construct Allocator by &, buffer_id: " << buffer_.GetID() << std::endl;
   }
 
   explicit Allocator(Buffer&& buffer) : buffer_(std::move(buffer)) {
-    std::cout << "construct Allocator by &&" << std::endl;
+    std::cout << "construct Allocator by &&, buffer_id: " << buffer_.GetID()
+              << std::endl;
   }
 
   Buffer& GetBuffer() { return buffer_; }
@@ -57,12 +68,8 @@ class Allocator {
   Buffer buffer_;
 };
 
-int main() {
-  std::string s1{"hello"};
-  std::string s2{std::move(s1)};
-  std::cout << s1.size() << std::endl;
-  std::cout << s2.size() << std::endl;
-
+void test_move() {
+  std::cout << "-------------test_move-----------" << std::endl;
   Buffer buf(100);
   buf.GetBuf()[2] = 'c';
   //  Buffer buf2 = buf;
@@ -71,6 +78,20 @@ int main() {
   std::cout << allocator.GetBuffer().GetBuf()[2] << std::endl;
   Buffer& alloc_buf = allocator.GetBuffer();
   alloc_buf.GetBuf()[2] = 'd';
-  Buffer b(1);
+}
+
+void test_rvalue() {
+  std::cout << "------------test_rvalue-----------" << std::endl;
+  Allocator allocator2(Buffer(2));
+  allocator2.GetBuffer();
+}
+
+int main() {
+  std::string s1{"hello"};
+  std::string s2{std::move(s1)};
+  std::cout << s1.size() << std::endl;
+  std::cout << s2.size() << std::endl;
+  test_move();
+  test_rvalue();
   return 0;
 }
